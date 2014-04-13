b87de05605582c9f8eaf61e6160bf93be7c8a6aa
Gcav66 <gustafcavanaugh@gmail.com>
2014-04-13 19:39:14 -0400

    busted hw 9

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,12 @@
 class Wine < ActiveRecord::Base
+valid_varietal = ["Merlot", "Chardonnay", "Moscato", "Classy", "Rose", "Chianti"]
+validates :name, :year, :winery, :country, :varietal, presence: true
+validates :year,
+   numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+   unless: "year.blank?"
+=begin
+validates :varietal,
+   :inclusion => { in: => valid_varietal]},
+   unless: "varietal.blank?"
+=end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 WineReview::Application.routes.draw do
-  resources :wines
+  resources :wines do
+      get 'page/:page', :action => :index, :on => :collection
+  end
+  root 'wines#index'
   #get "/wines" => "wines#index", as: 'wines'
   #get "/wines/:id" => "wines#show", as: 'wine'
   # The priority is based upon order of creation: first created -> highest priority.
